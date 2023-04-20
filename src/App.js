f3e064f0b7894580dfa86fded8fd51f0e54e99da
Samad <semed.semedzade@yahoo.com>
2023-04-20 21:04:28 +0400

    React-Gallery-With-Api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { Card, Image, Input, List, Space, Typography } from "antd";
+import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [searcedText, setSearcedText] = useState("");
+  const [dataSource, setDataSource] = useState([]);
+  const [loading, setloading] = useState(false);
+  const [previewImages, setPreviewImages] = useState([]);
+
+  useEffect(() => {
+    setloading(true);
+    fetch(`https://dummyjson.com/products/search?q=${searcedText}`)
+      .then((res) => res.json())
+      .then((response) => {
+        setDataSource(response.products);
+        setloading(false);
+      });
+  }, [searcedText]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Space style={{ padding: "0px 16px" }} direction="vertical">
+      <Typography.Title style={{ textAlign: "center" }}>
+        Gallery
+      </Typography.Title>
+      <Input.Search
+        style={{ maxWidth: 500, display: "flex", margin: "auto" }}
+        onSearch={(value) => {
+          setSearcedText(value);
+        }}
+      ></Input.Search>
+      <Typography.Text>
+        Showing results for:{" "}
+        <Typography.Text style={{ fontWeight: "bold" }}>
+          {searcedText || "ALL"}
+        </Typography.Text>
+      </Typography.Text>
+      <List
+        loading={loading}
+        dataSource={dataSource}
+        grid={{ xs: 1, sm: 2, md: 3, lg: 4, xl: 5, xxl: 6 }}
+        renderItem={(item) => {
+          return (
+            <Card
+              hoverable
+              key={item.id}
+              style={{
+                height: "auto",
+                // overflow: "hidden",
+                margin: "10px",
+              }}
+            >
+              <Image
+                src={item.thumbnail}
+                preview={{ visible: false }}
+                onClick={() => {
+                  setPreviewImages(item.images);
+                }}
+                style={{
+                  height: 300,
+                }}
+              ></Image>
+            </Card>
+          );
+        }}
+      />
+      {previewImages.length > 0 ? (
+        <Image.PreviewGroup
+          preview={{
+            visible: previewImages.length,
+            onVisibleChange: (value) => {
+              if (!value) {
+                setPreviewImages([]);
+              }
+            },
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {previewImages.map((image) => {
+            return <Image key={image} src={image} />;
+          })}
+        </Image.PreviewGroup>
+      ) : null}
+    </Space>
   );
-}
+};
 
 export default App;
